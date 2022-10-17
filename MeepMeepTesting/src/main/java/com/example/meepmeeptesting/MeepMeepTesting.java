cc19100370888d8416137a53fc5babbcc0aba976
William Pan <williamlpan22@gmail.com>
2022-10-16 17:24:15 -0700

    locations 1, 2, and 3 (also added waiting)

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -10,6 +10,7 @@ public class MeepMeepTesting {
         MeepMeep meepMeep = new MeepMeep(800);
 
         int location = 0;
+        //https://firstinspiresst01.blob.core.windows.net/first-energize-ftc/game-manual-part-2-traditional.pdf
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
@@ -18,17 +19,45 @@ public class MeepMeepTesting {
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(35, 62,Math.toRadians(-90) )) //starting position (x,y)
 
+                                //.waitSeconds(1) - stay still
+                                //each tile is 23.33 width
 
+
+                                //blue location 1 (left team)
                                 .forward(39)
                                 .turn(Math.toRadians(65))
                                 .forward(30)
                                 .turn(Math.toRadians(205))
                                 .turn(Math.toRadians(-90))
-                                .forward(30)
-
-
+                                .forward(24)
+                                .waitSeconds(2)
 //
-//                                .forward(20)
+//
+//                                //blue location 2 (left team)
+//                                .forward(39)
+//                                .turn(Math.toRadians(65))
+//                                .forward(30)
+//                                .turn(Math.toRadians(205))
+//                                .turn(Math.toRadians(-90))
+//                                .forward(24)
+//                                .turn(Math.toRadians(90))
+//                                .forward(24)
+//                                .waitSeconds(2)
+//
+//                                //blue location 3 (left team)
+//                                .forward(39)
+//                                .turn(Math.toRadians(65))
+//                                .forward(30)
+//                                .turn(Math.toRadians(205))
+//                                .turn(Math.toRadians(-90))
+//                                .forward(24)
+//                                .turn(Math.toRadians(90))
+//                                .forward(48)
+//                                .waitSeconds(2)
+
+
+
+
 
 
 
